88bd1ddbdcce43d035b570cf429ee0ece9f86b44
miragessee <miragessee@hotmail.com>
2016-12-05 23:28:15 +0200

    added DemoTaBarController v0.3
    added DemoTaBarController v0.3

--- AKSwiftSlideMenuDemoTabBar/AKSwiftSlideMenuDemoTabBar/SecondCellViewController.swift
+++ AKSwiftSlideMenuDemoTabBar/AKSwiftSlideMenuDemoTabBar/SecondCellViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class SecondCellViewController: BaseViewController {
 
+    @IBOutlet var screenEdgePanGesture: UIScreenEdgePanGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
+        screenEdgePanGesture.edges = .left
         addSlideMenuButton()
         // Do any additional setup after loading the view.
     }
@@ -21,6 +23,27 @@ class SecondCellViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func leftSlideAction(_ sender: Any) {
+        if (sender as AnyObject).state == .ended
+        {
+            BaseViewController.isLeftSlideAction = true
+            BaseViewController.isOpen = true
+            print("left slide")
+            
+            let menuVC : MenuViewController = self.storyboard!.instantiateViewController(withIdentifier: "MenuViewController") as! MenuViewController
+            menuVC.delegate = self
+            self.view.addSubview(menuVC.view)
+            self.addChildViewController(menuVC)
+            menuVC.view.layoutIfNeeded()
+            
+            
+            menuVC.view.frame=CGRect(x: 0 - UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height);
+            
+            UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                menuVC.view.frame=CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height);
+            }, completion:nil)
+        }
+    }
 
     /*
     // MARK: - Navigation
